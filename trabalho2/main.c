7677729ab19cf0a925012d3617ed56d686848712
José Flávio <jose.flavio@sou.unifal-mg.edu.br>
2022-08-10 14:29:13 -0300

    falta uns ajustes

--- trabalho2/main.c
+++ trabalho2/main.c
@@ -2,18 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM_DICT 4096
+#define NUM_SIMB 256
+
+typedef unsigned short int ui16;
+
+typedef struct alocaInfo {
+    ui16 *base;
+    int tam;
+    ui16 *proxAloca;
+} alocaInfo;
+
+int *pixels;
+
 int toB64(char c);
-unsigned long decToBin(int dec);
-int binToDec(unsigned long bin);
+unsigned long long decToBin(int dec);
+ui16 binToDec(unsigned long long bin);
 int pot2(int n);
+void iniciaAloca(alocaInfo *aloc, int tam);
+ui16 *aloca(alocaInfo *aloc, int len);
+void decodifica(ui16 *in, int n);
 
 int main(int argc, char **argv) {
+
+    int i, j, c, l, t;
+    ui16 *img;
+    unsigned long long ax, ay;
+    char aux;
+    char *lzw, *codigo;
 
     if (argc < 2) {
         exit(0);
     }
     
-    char *lzw = argv[1];
+    lzw = argv[1];
 
     FILE *f = fopen(lzw, "r");
 
@@ -23,38 +45,47 @@ int main(int argc, char **argv) {
 
     fscanf(f, "%s", lzw);
     
-    int c, l;
     fscanf(f, "%d %d", &c, &l);
-    char *codigo;
 
     codigo = (char *) malloc(sizeof(char) * c * l * 2);
-    fscanf(f, "%s", codigo);
-
-    while (fscanf(f, "%s", lzw) != EOF) {
-        strcat(codigo, lzw);
+    
+    i = 0;
+    while (fscanf(f, "%c", &aux) != EOF) {
+        if ((aux > 64 && aux < 91) ||
+            (aux > 96 && aux < 123) ||
+            (aux > 47 && aux < 58) ||
+            aux == '+' ||
+            aux == '/') {
+            codigo[i++] = aux;
+        } 
     }
 
-    int img[l * c];
-    int t = strlen(codigo);
+    codigo[i] = '\0';
+
+    t = strlen(codigo);
     printf("%d\n", t);
-    unsigned long ax, ay;
-    for (int i = 0, j = 0; i < t; i += 2, j++) {
+
+    img = (ui16 *) malloc(sizeof(ui16) * (t / 2));
+    pixels = (int *) malloc(sizeof(int) * c * l);
+    
+    for (i = 0, j = 0; i < t; i += 2, j++) {
         ax = decToBin(toB64(codigo[i])) * 1000000;
         ay = decToBin(toB64(codigo[i + 1]));   
         img[j] = binToDec(ax + ay);
-        //printf("%d\n", img[j]);
     }
-    
+  
     fclose(f);
+
+    decodifica(img, t / 2);
 
     f = fopen("saida.pgm", "w+");
 
     fprintf(f, "%s\n", "P2");
     fprintf(f, "%d %d\n", c, l);
-    fprintf(f, "%d\n", 4095);
-    //fprintf(f, "%s", codigo);
-    for (int i = 0; i < l * c; i++) {
-        fprintf(f, "%d ", img[i]);
+    fprintf(f, "%d\n", 255);
+    
+    for (i = 0; i < l * c; i++) {
+        fprintf(f, "%d ", pixels[i]);
     }
 
     fclose(f);
@@ -72,6 +103,10 @@ int toB64(char c) {
         return c - 97 + 26;
     }
 
+    if (c > 47 && c < 58) {
+        return c - 48 + 52;
+    }
+
     if (c == '+') {
         return 62;
     }
@@ -81,7 +116,7 @@ int toB64(char c) {
     }
 }
 
-unsigned long decToBin(int dec) {
+unsigned long long decToBin(int dec) {
     int bin = 0;
     int fator = 1;
     int r;
@@ -100,8 +135,8 @@ unsigned long decToBin(int dec) {
     return bin;
 }
 
-int binToDec(unsigned long bin) {
-    int dec = 0;
+ui16 binToDec(unsigned long long bin) {
+    ui16 dec = 0;
     int pot = 0;
     int resto;
 
@@ -124,4 +159,82 @@ int pot2(int n) {
     }
 
     return p;
+}
+
+void iniciaAloca(alocaInfo *aloc, int tam) {
+    aloc->base = malloc(tam);
+    aloc->tam = tam;
+    aloc->proxAloca = aloc->base;
+}
+
+ui16 *aloca(alocaInfo *aloc, int len) {
+    ui16 *ret = aloc->proxAloca;
+    aloc->proxAloca += len;
+    return ret;
+}
+
+void decodifica(ui16 *in, int n) {
+    struct {
+        ui16 *seq;
+        int tam;
+    } dicionario[TAM_DICT];
+
+    alocaInfo aInfo;
+    ui16 *marca;
+    int posDict;
+    int anterior;
+    int i;
+    int auxP = 0;
+
+    iniciaAloca(&aInfo, TAM_DICT * TAM_DICT * sizeof(ui16));
+    marca = aInfo.proxAloca;
+
+    for (int i = 0; i < NUM_SIMB; i++) {
+        dicionario[i].seq = aloca(&aInfo, 1);
+        dicionario[i].seq[0] = i;
+        dicionario[i].tam = 1;
+    }
+
+    posDict = NUM_SIMB;
+
+    anterior = in[0];
+    pixels[auxP++] = anterior;
+    i = 1;
+
+    while (i < n) {
+        int simbolo = in[i++];
+        if (posDict == TAM_DICT) {
+            aInfo.proxAloca = marca;
+
+            for (int j = 0; j < NUM_SIMB; j++) {
+                dicionario[j].seq = aloca(&aInfo, 1);
+                dicionario[j].seq[0] = j;
+                dicionario[j].tam = 1;
+            }
+
+            posDict = NUM_SIMB;
+        } else {
+            int tam = dicionario[anterior].tam;
+            
+            dicionario[posDict].tam = tam + 1;
+            dicionario[posDict].seq = aloca(&aInfo, tam + 1);
+
+            for (int k = 0; k < tam; k++) {
+                dicionario[posDict].seq[k] = dicionario[anterior].seq[k];
+            }
+
+            if (simbolo == posDict) {
+                dicionario[posDict++].seq[tam] = dicionario[anterior].seq[0];
+            } else {
+                dicionario[posDict++].seq[tam] = dicionario[simbolo].seq[0];
+            }
+        }
+
+        for (int k = 0; k < dicionario[simbolo].tam; k++) {
+            pixels[auxP++] = dicionario[simbolo].seq[k];
+        }
+
+        anterior = simbolo;
+    }
+    free(aInfo.base);
 }
